f7dd5978aea12b4d76d54a485fe657455bd7ea41
Maks Kayun <maks.kayun@yandex.ru>
2023-08-08 11:50:49 +0300

    rebase template

--- TCMS/App.xaml.cs
+++ TCMS/App.xaml.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
+using static TCMS.Startup;
 
 namespace TCMS
 {
@@ -13,5 +8,9 @@ namespace TCMS
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            Init();
+        }
     }
 }
--- TCMS/MainWindow.xaml.cs
+++ TCMS/MainWindow.xaml.cs
@@ -1,18 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using Microsoft.Extensions.DependencyInjection;
+﻿using System.Windows;
 
 namespace TCMS
 {
@@ -23,11 +9,8 @@ namespace TCMS
     {
         public MainWindow()
         {
+            Resources.Add("services", Startup.Services);
             InitializeComponent();
-            
-            var serviceCollection = new ServiceCollection();
-            serviceCollection.AddWpfBlazorWebView();
-            Resources.Add("services", serviceCollection.BuildServiceProvider());
         }
     }
 }
